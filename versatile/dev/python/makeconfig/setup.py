44d0bfdb4b03435a7119cc5f96c1f5ad0b15fe60
Liam Collod <lcollod@gmail.com>
2021-11-30 23:29:53 +0100

    created setup to define package variables

---
+++ versatile/dev/python/makeconfig/setup.py
@@ -0,0 +1,11 @@
+"""
+
+"""
+import logging
+
+logger = logging.getLogger("mkc.setup")
+
+
+NUM_ROUND = 12  # number of decimals values to keep for config numbers.
+
+CAT = "Bradford"  # chromatic adaption transform used for conversions.
